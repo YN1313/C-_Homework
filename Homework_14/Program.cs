27f525ae7eff814477b86f29bdd9a9e779650d99
DALER DADADZHANOVe <d.dadadzhanov87@gmail.com>
2023-07-20 00:28:40 +0600

    Homework_014

---
+++ Homework_14/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] ourNumb = new int[sizeArray];
+FillArrayRandomNumbers(ourNumb);
+Console.WriteLine("Наш массив: ");
+PrintArray(ourNumb);
+int count = 0;
+
+for (int i = 0; i < ourNumb.Length; i++)
+  if (ourNumb[i] % 2 == 0)
+    count++;
+
+Console.WriteLine($"В массиве {count} чётных чисел");
+
+void FillArrayRandomNumbers(int[] ourNumb)
+{
+  for (int j = 0; j < ourNumb.Length; j++)
+  {
+    ourNumb[j] = new Random().Next(100, 1000);
+  }
+}
+
+void PrintArray(int[] ourNumb)
+{
+  Console.Write("[ ");
+  for (int j = 0; j < ourNumb.Length; j++)
+  {
+    Console.Write(ourNumb[j] + " ");
+  }
+  Console.Write("]");
+  Console.WriteLine();
+}
