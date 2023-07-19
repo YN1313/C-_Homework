e7bc40550d909f117351f4b0863a48cf522f0350
DALER DADADZHANOVe <d.dadadzhanov87@gmail.com>
2023-07-20 00:29:28 +0600

    Homework_016

---
+++ Homework_16/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] ourNumb = new int[sizeArray];
+FillArrayRandomNumbers(ourNumb);
+Console.WriteLine("Наш массив: ");
+PrintArray(ourNumb);
+int sumOfNumbers = 0;
+
+for (int i = 0; i < ourNumb.Length; i += 2)
+  sumOfNumbers += ourNumb[i];
+
+Console.WriteLine($"Cумма элементов на нечётных позициях = {sumOfNumbers}");
+
+void FillArrayRandomNumbers(int[] ourNumb)
+{
+  for (int j = 0; j < ourNumb.Length; j++)
+  {
+    ourNumb[j] = new Random().Next(1, 20);
+  }
+}
+
+void PrintArray(int[] ourNumb)
+{
+  Console.Write("[ ");
+  for (int j = 0; j < ourNumb.Length; j++)
+  {
+    Console.Write(ourNumb[j] + " ");
+  }
+  Console.Write("]");
+  Console.WriteLine();
+}
